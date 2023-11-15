705aee94a43c54217e657a4e428330226c00231b
Steve Massey <62776118+stmassey84@users.noreply.github.com>
2023-11-15 18:52:12 -0500

    Delete open_gate.sh

--- open_gate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Send the open gate command to /dev/ttyUSB0
-echo -e "\xA0\x01\x01\xA2" > /dev/ttyUSB0
-
-# Wait briefly to allow the gate to open
-sleep .5
-
-echo "Gate is now open."
