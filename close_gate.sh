99403079fac82ef640db9ef7b0f0b95f31423203
Steve Massey <62776118+stmassey84@users.noreply.github.com>
2023-11-15 18:51:56 -0500

    Delete close_gate.sh

--- close_gate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Send the close gate command to /dev/ttyUSB0
-echo -e "\xA0\x01\x00\xA1" > /dev/ttyUSB0
-
-# Wait briefly to allow the gate to close
-sleep .5
-
-echo "Gate is now closed."
